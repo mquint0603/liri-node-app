c50e46f0bc0ee585248e5e6eeaa5c181f1fcd94f
Mary Quint <mary.k.quint@gmail.com>
2018-04-22 22:20:03 -0500

    added log

--- liri.js
+++ liri.js
@@ -1,3 +1,5 @@
+// Set up ----------------------------------------------------------------------------------------------------------------------------------------------
+
 require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
@@ -8,6 +10,8 @@ var client = new Twitter(keys.twitter);
 var fs = require("fs");
 var request = require("request");
 
+
+//Take in user input ----------------------------------------------------------------------------------------------------------------------------------------------
 var args = process.argv;
 var command = args[2];
 var media = '';
@@ -22,58 +26,36 @@ for (var i = 3; i < args.length; i++) {
   }
 
 
-  switch (command) {
-    case "my-tweets":
-        var params = {
-            screen_name: 'QuintyQuints',
-            count: 20,
-            result_type: 'recent',
+// Run app ----------------------------------------------------------------------------------------------------------------------------------------------
+function doStuff (){
+    switch (command) {
+      case "my-tweets":
+          showTweets()
+          log()
+          break;
+      case "spotify-this":
+          if (media === '' && command === "spotify-this"){
+              media = 'the sign ace of base'
+          } 
+          song()
+          log()
+          break;
+      case "movie-this":
+          if (media === ''){
+              media = 'mr nobody'
           }
-          client.get('statuses/user_timeline', params, function(err, data, response) {
-            if(!err){
-              for (let i = 0; i < data.length; i++){
-                  console.log(data[i].created_at, ':\n' + data[i].text + '\n')
-              }
-            } else {
-              return console.log(err);
-            }
-          })
-        break;
+          movie() 
+          log()       
+          break;
+      case "do-what-it-says":
+          doWhatitSays()
+    }
+}
 
-    case "do-what-it-says":
-        fs.readFile("random.txt", "utf8", function(err, data){
-            if (err) {
-                return console.log(err);
-            }
-            var defaultItems = data.split(",")
-            media = defaultItems[1]
-            song()
-        });
-        break;
+doStuff()
 
-    case "spotify-this":
-        if (media === '' && command === "spotify-this"){
-            media = 'the sign ace of base'
-        } 
-        song()
-        break;
 
-    case "movie-this":
-        if (media === ''){
-            media = 'mr nobody'
-        }
-        var queryUrl = "http://www.omdbapi.com/?t=" + media + "&y=&plot=short&apikey=trilogy";
-        request(queryUrl, function(error, response, body) {
-            if (!error && response.statusCode === 200) {
-              console.log("Title: " + JSON.parse(body).Title + "\nRelease Year: " + JSON.parse(body).Year +
-                "\nRating: " + JSON.parse(body).Rated + "\nRotten Tomatoes Score: " + JSON.parse(body).Ratings[1].Value
-                + "\nLanugage: " + JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot + "\nActors: " + JSON.parse(body).Actors);
-            }
-          });
-        
-        break;
-  }
-
+//Functionality  ----------------------------------------------------------------------------------------------------------------------------------------------
   function song () {
     spotify.search({ type: 'track', query: media }, function (err, data) {
         if (!err) {
@@ -91,5 +73,48 @@ for (var i = 3; i < args.length; i++) {
     });
   }
   function movie (){
+    var queryUrl = "http://www.omdbapi.com/?t=" + media + "&y=&plot=short&apikey=trilogy";
+        request(queryUrl, function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+              console.log("Title: " + JSON.parse(body).Title + "\nRelease Year: " + JSON.parse(body).Year +
+                "\nRating: " + JSON.parse(body).Rated + "\nRotten Tomatoes Score: " + JSON.parse(body).Ratings[1].Value
+                + "\nLanugage: " + JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot + "\nActors: " + JSON.parse(body).Actors);
+            }
+          });
+  }
+  function doWhatitSays (){
+    fs.readFile("random.txt", "utf8", function(err, data){
+        if (err) {
+            return console.log(err);
+        }
+        var defaultItems = data.split(",")
+        command = defaultItems[0]
+        media = defaultItems[1]
+        doStuff()
+    });
+  }
+  function showTweets (){
+    var params = {
+        screen_name: 'QuintyQuints',
+        count: 20,
+        result_type: 'recent',
+      }
+      client.get('statuses/user_timeline', params, function(err, data, response) {
+        if(!err){
+          for (let i = 0; i < data.length; i++){
+              console.log(data[i].created_at, ':\n' + data[i].text + '\n')
+          }
+        } else {
+          return console.log(err);
+        }
+      })
+  }
+  function log (){
+    fs.appendFile("log.txt", [`${command}  ${media} \n`], function(err) {
+        if (err) {
+          return console.log("error");
+        }
+        // console.log("log.txt was updated!");
       
+      });
   }
